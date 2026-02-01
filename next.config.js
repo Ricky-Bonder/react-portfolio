cee52faf76e78eb8445df2d864b57ffa55b18b65
Riccardo Ossola <rickyossola@yahoo.it>
2026-02-01 02:25:13 +0100

    Set basePath based on production environment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: isProd ? '/react-portfolio' : '',
   output: 'export',
   images: { unoptimized: true },
 };
